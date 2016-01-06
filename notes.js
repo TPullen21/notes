b4e8e9e4b06b01f1cee9ce8992cc978fd4f2b77d
Tom Pullen <tpullen21@gmail.com>
2016-01-06 10:03:25 +0000

    Initial commit with ability to log in, add and update notes

---
+++ notes.js
@@ -0,0 +1,154 @@
+Notes = new Mongo.Collection("notes");
+ 
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish("notes", function () {
+    return Notes.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
+  });
+}
+ 
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Meteor.subscribe("notes");
+
+  Template.body.helpers({
+    notes: function () {
+        return Notes.find({}, {sort: {createdAt: -1}});
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    currentNoteText: function () {
+      return Session.get("currentNoteText");
+    },
+    currentNoteTitle: function () {
+      return Session.get("currentNoteTitle");
+    },
+    currentNoteId: function () {
+      return Session.get("currentNoteId");
+    }
+  });
+
+  Template.body.events({
+    "submit .new-note": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get values from form element
+      var title = event.target.title.value;
+      var text = event.target.text.value;
+ 
+      // Insert a note into the collection
+      Meteor.call("addNote", title, text);
+ 
+      // Clear form
+      event.target.text.value = "";
+    },
+    "submit .update-note": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get values from form element
+      var title = event.target.title.value;
+      var text = event.target.text.value;
+ 
+      // Insert a note into the collection
+      Meteor.call("updateNote", Session.get("currentNoteId"), title, text);
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
+    },
+    "mouseover .notePreview": function (event) {
+      Session.set("currentNoteText", this.text);
+      Session.set("currentNoteTitle", this.title);
+      Session.set("currentNoteId", this._id);
+    }
+  });
+ 
+  Template.note.helpers({
+    isOwner: function () {
+      return this.owner === Meteor.userId();
+    }
+  });
+
+  Template.registerHelper('trunc', function(passedString) {
+    var text = passedString.substring(0,50); //same as truncate.
+    return new Spacebars.SafeString(text)
+  });
+ 
+  Template.note.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Meteor.call("setChecked", this._id, ! this.checked);
+    },
+    "click .delete": function () {
+      Meteor.call("deleteNote", this._id);
+    },
+    "click .toggle-private": function () {
+      Meteor.call("setPrivate", this._id, ! this.private);
+    }
+  });
+ 
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+}
+ 
+Meteor.methods({
+  addNote: function (title, text) {
+    // Make sure the user is logged in before inserting a note
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+ 
+    Notes.insert({
+      title: title,
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteNote: function (noteId) {
+    var note = Notes.findOne(noteId);
+    if (note.private && note.owner !== Meteor.userId()) {
+      // If the note is private, make sure only the owner can delete it
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Notes.remove(noteId);
+  },
+  updateNote: function (noteId, title, text) {
+    var note = Notes.findOne(noteId);
+    if (note.owner !== Meteor.userId()) {
+      // If the note is private, make sure only the owner can check it off
+      throw new Meteor.Error("not-authorized");
+    }
+    
+    Notes.update(noteId, { $set: { text: text, title: title } });
+  },
+  setChecked: function (noteId, setChecked) {
+    var note = Notes.findOne(noteId);
+    if (note.private && note.owner !== Meteor.userId()) {
+      // If the note is private, make sure only the owner can check it off
+      throw new Meteor.Error("not-authorized");
+    }
+    
+    Notes.update(noteId, { $set: { checked: setChecked} });
+  },
+  setPrivate: function (noteId, setToPrivate) {
+    var note = Notes.findOne(noteId);
+ 
+    // Make sure only the note owner can make a note private
+    if (note.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+ 
+    Notes.update(noteId, { $set: { private: setToPrivate } });
+  }
+});
